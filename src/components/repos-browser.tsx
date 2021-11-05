8110192796829c36b911f1f037ac9fbfd24bcf06
v.shustov <57774085+shuimi@users.noreply.github.com>
2021-11-05 18:41:02 +0300

    -repoboard: search with pagination implemented -api errors handling added

--- src/components/repos-browser.tsx
+++ src/components/repos-browser.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Col, Divider, Empty, Input, Pagination, Row, Spin } from "antd";
+import { Alert, Col, Divider, Empty, Input, Pagination, Row, Spin } from "antd";
 import RepositoryCard from "./repository-card";
 import { octokit } from "../services/octokit";
 
@@ -28,6 +28,7 @@ const ReposBrowser = () => {
         totalCount: 0,
         items: null
     });
+    const resetRepos = () => setRepos({ totalCount: 0, items: null });
 
     const [ searchQuery, setSearchQuery ] = useState<string>('');
 
@@ -39,7 +40,8 @@ const ReposBrowser = () => {
     const [ fetchingError, setFetchingError ] = useState('');
 
     useEffect(() => {
-        searchQuery && setFetching(true) || setRepos({ totalCount: 0, items: null });
+        setFetchingError('');
+        searchQuery && setFetching(true);
         searchQuery && octokit.rest.search.repos({
             q: searchQuery.split(' ').reduce((aggregation, topic) => aggregation += `topic:${ topic }+`),
             order: 'asc',
@@ -51,7 +53,7 @@ const ReposBrowser = () => {
                 setRepos({ totalCount: repos.data.total_count, items: repos.data.items && repos.data.items || null });
             })
             .catch(error => {
-                console.log(error);
+                setFetchingError(error.code + ' ' + error.message);
             })
             .finally(() => {
                 setFetching(false);
@@ -66,26 +68,43 @@ const ReposBrowser = () => {
         } }>
             <Input.Search onSearch={ onSearchInput } size="large"
                           placeholder="Search for repos"/>
-            <Divider orientation="left">{ repos && 'Found: ' + repos.totalCount || 'Repositories' }</Divider>
+            {
+                !fetchingError && <Divider orientation="left">{ repos && 'Found: ' + repos.totalCount || 'Repositories' }</Divider>
+            }
             <Row style={ {
                 justifyContent: 'center',
             } } gutter={ { xs: 1, sm: 2, md: 3, lg: 3 } }>
-                { fetching && <Spin/> }
                 {
-                    repos.items
-                    && repos.items.map(repo => <RepositoryCard key={ repo.id } name={ repo.name } loading={ false }/>)
+                    fetchingError && <Alert
+                        message="Error loading data!"
+                        description={fetchingError}
+                        type="error"
+                        closable
+                    />
+                }
+                { !fetchingError && fetching && <Spin/> }
+                {
+                    !fetchingError &&
+                    !fetching &&
+                    repos.items &&
+                    repos.items.map(repo => <RepositoryCard key={ repo.id } name={ repo.name } loading={ false }/>)
                     ||
-                    !fetching
+                    !fetchingError && !fetching
                     && <Empty description={ false }/>
                 }
             </Row>
-            Only first 1000 results available ({ 1000 / pagination.pageSize } pages)
             {
-                repos.items && repos.items.length > 1 &&
-                <Pagination total={ repos.totalCount >= 1000 ? 1000 : repos.totalCount }
-                            pageSizeOptions={ pageSizeOptions }
-                            onChange={ onPageChange }
-                            showSizeChanger/>
+                !fetchingError &&
+                repos.items &&
+                repos.items.length > 1
+                &&
+                <>
+                    Only first 1000 results available ({ 1000 / pagination.pageSize } pages)
+                    <Pagination total={ repos.totalCount >= 1000 ? 1000 : repos.totalCount }
+                                pageSizeOptions={ pageSizeOptions }
+                                onChange={ onPageChange }
+                                showSizeChanger/>
+                </>
             }
 
         </section>
